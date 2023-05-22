a94f2f3ae4f0e525fff9b38575f26675ef211f22
Smile-112 <Lnowikow@mail.ru>
2023-05-22 11:35:21 +0300

    Adding Lesson_2 to Python_Developer

---
+++ Lesson2/variables.py
@@ -0,0 +1,6 @@
+name = "Land Rover"
+seats = 5
+eng_cons = 5.7
+on_sale = True
+print(f"Название: {name}\t\t{type(name)}\nКоличество мест: {seats}"
+      f"\t\t\t{type(seats)}\nПотребление двигателя: {eng_cons}\t{type(eng_cons)}\nВ продаже: {on_sale}\t\t\t\t{type(on_sale)}")
